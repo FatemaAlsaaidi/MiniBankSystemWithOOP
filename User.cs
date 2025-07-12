652ed79cb3fde3e44a9e50b056586aba5d7e4d35
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-07-12 12:06:19 +0400

    Fields with encapsulation in User class

--- User.cs
+++ User.cs
@@ -8,5 +8,16 @@ namespace MiniBankSystemWithOOP
 {
     class User
     {
+        
+        // Fields with encapsulation
+        public int AccountNumber { get; private set; }
+        public string Name { get; private set; }
+        public string NationalID { get; private set; }
+        public double Balance { get; private set; }
+        public string PhoneNumber { get; private set; }
+        public string Address { get; private set; }
+        public bool HasActiveLoan { get; private set; }
+        public double LoanAmount { get; private set; }
+        public double LoanInterestRate { get; private set; }
     }
 }
