b2c7282247de39041812409d5fdbbdff2d3c7fcb
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-07-12 12:09:23 +0400

    Create Parameterized Constructor

--- User.cs
+++ User.cs
@@ -19,5 +19,19 @@ namespace MiniBankSystemWithOOP
         public bool HasActiveLoan { get; private set; }
         public double LoanAmount { get; private set; }
         public double LoanInterestRate { get; private set; }
+
+        // Parameterized Constructor
+        public User(int accountNumber, string name, string nationalID, double initialBalance, string phone, string address)
+        {
+            AccountNumber = accountNumber;
+            Name = name;
+            NationalID = nationalID;
+            Balance = initialBalance;
+            PhoneNumber = phone;
+            Address = address;
+            HasActiveLoan = false;
+            LoanAmount = 0;
+            LoanInterestRate = 0;
+        }
     }
 }
