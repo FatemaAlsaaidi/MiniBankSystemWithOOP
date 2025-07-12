8da8ad0df8e01077eb3c09df42015cc5b238c042
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-07-12 11:10:09 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniBankSystemWithOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
