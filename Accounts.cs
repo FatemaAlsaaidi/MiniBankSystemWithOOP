6effd9ffaf8a6ed36126ec6001a2384bf30b402f
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-07-15 01:00:40 +0400

    Properties for account details

--- Accounts.cs
+++ Accounts.cs
@@ -9,16 +9,27 @@ namespace MiniBankSystemWithOOP
     class Accounts
     {
         // Properties for account details
-        public static string AccountNumber ;
+        private static string accountNumber ;
         private static int AccountCounter=0;
-        public string Name { get; set;}
-        private string NationalID;
-        private string Password;
-        public string PhoneNumber;
-        private double Balance;
-        public bool IsActivity;
-        public string Type;
-        public string Address;
+        private string name;
+        private string nationalID;
+        private string password;
+        private string phoneNumber;
+        private double balance;
+        private bool isActivity;
+        private string type;
+        private string address;
+
+        // Properties for account details
+        public string Name { get { return name; } set { name = value; } }
+        public string NationalID { get { return nationalID; } set { nationalID = value; } }
+        public string Password { set {password = value; } }
+        public string PhoneNumber { get { return phoneNumber; } set { phoneNumber = value; } }
+        public double Balance1 { get { return balance; } set { balance = value; } }
+        public bool IsActivity1 { get { return isActivity; } set { isActivity = value; } }
+        public string AccountNumber1 { get { return accountNumber; } set { accountNumber = value; } }
+        public string Type1 { get { return type; } set { type = value; } }
+        public string Address1 { get { return address; } set { address = value; } }
 
         // Defualt Constructor
         public Accounts()
